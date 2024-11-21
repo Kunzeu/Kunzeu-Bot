2ed7a695c0af97cc39f9f0c39412baec8582511c
Kunzeu <kunjohn24@gmail.com>
2024-11-21 12:39:48 -0500

    Items

--- commands/fun/recordar.js
+++ commands/fun/recordar.js
@@ -1,8 +1,8 @@
 const cron = require('node-cron');
 
 module.exports = (client) => {
-    // Programar el recordatorio para cada jueves a las 12:15 PM (Colombia)
-    cron.schedule('30 12 * * 4', () => {
+    // Programar el recordatorio para cada lunes a las 2:00 PM (Colombia)
+    cron.schedule('0 2 * * 1', () => {
         const channel = client.channels.cache.get('1178687540232978454'); // Reemplaza con el ID del canal donde deseas enviar el mensaje
         const roleId = '1239969056099012628'; // Reemplaza con el ID del rol que deseas mencionar
 
